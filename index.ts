9029e01baa6816e35f8a46e9a1fdcfd20b3b6b35
kkuejo <kkuejo@yahoo.co.jp>
2025-07-10 00:24:24 +0200

    index.tsにboolean, number, string

---
+++ index.ts
@@ -0,0 +1,12 @@
+//letでデータを定義
+//hasvalueが変数名、booleanが型(コロン、スペース)
+let hasValue: boolean = true;
+//number型は浮動小数点
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+//stringは文字列、シングルクオーテーション、ダブルクオーテーション、バッククオーテーションでも良い
+let single: string = 'hello';
+let doublle: string = "hello";
+let back: string = `hello`;
+
