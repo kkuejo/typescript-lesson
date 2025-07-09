3d8c0da0c54ce96d511f1a10f356ca0768bc00b0
kkuejo <kkuejo@yahoo.co.jp>
2025-07-10 00:34:23 +0200

    型推論

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
 //letでデータを定義
 //hasvalueが変数名、booleanが型(コロン、スペース)
+//コロン、スペースの後に型を書くのが型注釈
+//型注釈はオプションであり、型推論が可能な場合は省略可能
 let hasValue: boolean = true;
+let hasValue2 = true;
+
 //number型は浮動小数点
 let count: number = 10;
 let float: number = 3.14;
 let negative: number = -0.12;
+
 //stringは文字列、シングルクオーテーション、ダブルクオーテーション、バッククオーテーションでも良い
 let single: string = 'hello';
 let doublle: string = "hello";
